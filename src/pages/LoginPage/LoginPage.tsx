0e99becc309f51b5c6a688be711b56a330b6bf7f
Azizbek Samatov <azizbeksamatov98@gmail.com>
2023-01-26 00:02:18 +0500

    feat: working on development of login page of the project

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../../assets/logo.svg';
 import './LoginPage.scss';
 
 const LoginPage: React.FC = () => {
@@ -9,16 +10,23 @@ const LoginPage: React.FC = () => {
   return (
     <div className="login-container">
       <form onSubmit={handleSubmit} className="login-form">
-        <h1>Log In</h1>
-        <label htmlFor="user">User ID:</label>
-        <input id="user" />
-        <label htmlFor="password">Password:</label>
-        <input id="password" />
+        <h1 className="login-title">Log In</h1>
+        <label className="login-label" htmlFor="user">
+          User ID:
+        </label>
+        <input className="login-input" id="user" />
+        <label className="login-label" htmlFor="password">
+          Password:
+        </label>
+        <input className="login-input" id="password" />
         <div>
           <button>Reset</button>
           <button>Log In</button>
         </div>
       </form>
+      <div className="login-footer">
+        <img src={logo} alt="logo" height="25" />
+      </div>
     </div>
   );
 };
