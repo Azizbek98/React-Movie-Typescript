ee4be4ce8635e7f69222df7d75e3b814ef5a2f15
Azizbek Samatov <azizbeksamatov98@gmail.com>
2023-01-25 23:44:28 +0500

    feat: working on development of login page of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import HomePage from './pages/HomePage/HomePage';
+// import HomePage from './pages/HomePage/HomePage';
 import ErrorBoundary from './ErrorBoundary';
 import './App.scss';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 const App: React.FC = () => {
   return (
     <>
       <ErrorBoundary>
-        <HomePage />
+        <LoginPage />
       </ErrorBoundary>
     </>
   );
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import './LoginPage.scss';
 
 const LoginPage: React.FC = () => {
-  return <div>LoginPage</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h1>Log In</h1>
+        <label htmlFor="user">User ID:</label>
+        <input id="user" />
+        <label htmlFor="password">Password:</label>
+        <input id="password" />
+        <div>
+          <button>Reset</button>
+          <button>Log In</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
